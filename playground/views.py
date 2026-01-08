548b284866e7ed274bb4b86636e6282781269c7b
Md. Imran Hasan <imranhasan@ad.enosisbd.com>
2026-01-08 16:37:17 +0600

    Practice how to configure templates and debugger

--- playground/views.py
+++ playground/views.py
@@ -7,4 +7,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    x = 1
+    y = x
+    z = x + 1
+    return render(request, "hello.html", {"name":"Imran"})
