784b0b7ec2dce52c8316ac7a2253e695ef17bf26
Md. Imran Hasan <imranhasan@ad.enosisbd.com>
2026-01-08 15:43:38 +0600

    Setup views and urls for an endpoint

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# request -> responce
+# request handler
+
+from django.http import HttpResponse
+
+def say_hello(request):
+    return HttpResponse('Hello World')
